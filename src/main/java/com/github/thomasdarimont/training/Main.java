b665d46c1b16a3771919b1996232701d5f33a3e6
Thomas Darimont <thomas.darimont@googlemail.com>
2020-11-25 20:15:45 +0100

    Initial import

---
+++ src/main/java/com/github/thomasdarimont/training/Main.java
@@ -0,0 +1,10 @@
+package com.github.thomasdarimont.training;
+
+import com.github.thomasdarimont.training.greeting.Greeter;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Greeter greeter = null;
+    }
+}
